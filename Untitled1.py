34710662955a0ee7033c9d80e061b988d0829b2f
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-18 19:16:05 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -543,11 +543,15 @@ temp_df
 
 import plotly.graph_objects as go
 fig = go.Figure()
-#wicket=fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['wickets_in_over'], mode='markers',name='Wickets in Over', marker=dict(color='yellow')))
-batting_team=fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines',name='Batting side', line=dict(color='#00a65a', width=3)))
-bowling_team=fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines',name='Bowling Side', line=dict(color='red', width=4)))
-runs=fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'],name='Runs in Over'))
+fig.data = [
+    go.Scatter(x=temp_df['end_of_over'], y=temp_df['wickets_in_over'], mode='markers', name='Wickets in Over', marker=dict(color='yellow')),
+    go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting side', line=dict(color='#00a65a', width=3)),
+    go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name='Bowling Side', line=dict(color='red', width=4)),
+    go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over')
+]
+
 fig.update_layout(title='Target-' + str(target))
+
 import streamlit as st
 st.write(fig)
 
