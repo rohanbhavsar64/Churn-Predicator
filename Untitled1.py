0bc8bedc0f0af66a1e79e22964a3e5e2d835258e
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-18 19:19:26 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -550,7 +550,6 @@ fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='line
 fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over'))
 
 fig.update_layout(title='Target-' + str(target))
-
 import streamlit as st
 st.write(fig)
 
