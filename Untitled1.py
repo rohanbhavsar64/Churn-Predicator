5f27a1618ee4c780b3a06c5aed8cc7aacbf59a6d
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-18 20:06:11 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -574,6 +574,15 @@ else:
                                             line=dict(color='red', width=4)))
         fig.update_layout(title='Target-' + str(target))
         st.write(fig)
+        r1 = match[match['id'] == l]['player_of_match'].unique()
+        r2 = match[match['id'] == l]['winner'].unique()
+        r3 = match[match['id'] == l]['venue'].unique()
+        r4 = match[match['id'] == l]['city'].unique()
+        r5 = match[match['id'] == l]['toss_winner'].unique()
+        r6 = match[match['id'] == l]['dl_applied'].unique() 
+        data = {'Field': ['Vanue', 'City', 'Toss Winner', 'DLS Methode', 'POM', 'Winner'], 'Name': [r3[0], r4[0], r5[0], r6[0], r1[0], r2[0]]} 
+        fg = pd.DataFrame(data) 
+        st.table(fg)
 
 
 
