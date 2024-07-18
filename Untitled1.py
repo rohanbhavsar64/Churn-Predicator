a54b5d7727eac872284580d64baf07fba6c0d689
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-18 19:37:21 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -559,7 +559,7 @@ fig = go.Figure()
 fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['wickets_in_over'], mode='markers', name='Wickets in Over', marker=dict(color='yellow')))
 fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting side', line=dict(color='#00a65a', width=3)))
 fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name='Bowling Side', line=dict(color='red', width=4)))
-fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over'))
+fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over', marker=dict(color='purple')))
 
 fig.update_layout(title='Target-' + str(target))
 
