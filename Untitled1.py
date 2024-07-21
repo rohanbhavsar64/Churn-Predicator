a04a8a64900ebdef47c17b7b953a324d6c060779
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-21 15:51:34 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -554,7 +554,8 @@ else:
     if temp_df is None:
         st.write("Error: Match is not Existed")
     else:
-        st.write(temp_df.iloc[:49][:])
+        st.write(temp_df.iloc[:48][:])
+temp_df=temp_df.iloc[:48][:]
 
 # In[271]:
 import plotly.graph_objects as go
