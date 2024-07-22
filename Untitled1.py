5887d97d32e7c6e8dd497e5baac8d913e6624aa1
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-22 15:28:06 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -596,7 +596,7 @@ else:
 # Area plots for batting and bowling teams
         batting_team = fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='none', fill='tozeroy', name='Batting Team',
                                         fillcolor='green',line=dict(color='#00a65a', width=3)))
-        bowling_team = fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='none', fill='tozeroy', name='Bowling Team',
+        bowling_team = fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='none', fill='tonexty', name='Bowling Team',
                                                  fillcolor='red',line=dict(color='red', width=4)))
 
         st.write(fig1)
