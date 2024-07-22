4a77447b7a9c2cfc080cde68b37d5e57fde749e9
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-22 16:37:43 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -580,29 +580,29 @@ else:
         fg = pd.DataFrame(data) 
         st.table(fg)
 
-        fig = go.Figure()
-        runs = fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over',marker=dict(color='purple')))
-        wicket_text = temp_df['wickets_in_over'].astype(str)
-        wicket_y = temp_df['runs_after_over'] + temp_df['wickets_in_over'] * 1  # adjust y-position based on wickets
-        wicket_y[wicket_y == temp_df['runs_after_over']] = None  # hide scatter points for 0 wickets
+        #fig = go.Figure()
+        #runs = fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over',marker=dict(color='purple')))
+        #wicket_text = temp_df['wickets_in_over'].astype(str)
+        #wicket_y = temp_df['runs_after_over'] + temp_df['wickets_in_over'] * 1  # adjust y-position based on wickets
+        #wicket_y[wicket_y == temp_df['runs_after_over']] = None  # hide scatter points for 0 wickets
 
-        wicket = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=wicket_y,  # use adjusted y-position
+        #wicket = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=wicket_y,  # use adjusted y-position
                                             mode='markers', name='Wickets in Over',
                                             marker=dict(color='orange', size=10),
                                             text=wicket_text, textposition='top center'))
 
 # Line plots for batting and bowling teams
-        batting_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting Team',
+        #batting_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting Team',
                                             line=dict(color='#00a65a', width=3)))
-        bowling_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name='Bowling Team',
+        #bowling_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name='Bowling Team',
                                             line=dict(color='red', width=4)))
-        fig.update_layout(
-            title='Target-' + str(target),
-            width=800,  # Set the width of the chart
-            height=700  # Set the height of the chart
-        )
-        fig.update_layout(title='Target-' + str(target))
-        st.write(fig) 
+        #fig.update_layout(
+          #  title='Target-' + str(target),
+         #   width=800,  # Set the width of the chart
+        #    height=700  # Set the height of the chart
+       # )
+        #fig.update_layout(title='Target-' + str(target))
+       # st.write(fig) 
 
         fig1 = go.Figure()
         
