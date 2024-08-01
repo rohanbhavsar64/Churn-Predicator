ea9e1d1013e736b4c5328e8f534e034d13746e82
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-01 19:16:59 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -503,14 +503,64 @@ def match_progression(x_df,match_id,pipe):
 
 
 # In[270]:
+import pandas as pd 
+from bs4 import BeautifulSoup
+import requests 
+r=requests.get('https://www.espncricinfo.com/series/icc-cricket-world-cup-2023-24-1367856/australia-vs-south-africa-2nd-semi-final-1384438/match-overs-comparison')
+#r1=requests.get('https://www.espncricinfo.com/series/icc-cricket-world-cup-2023-24-1367856/india-vs-new-zealand-1st-semi-final-1384437/full-scorecard')
+b=BeautifulSoup(r.text,'html')
+bowling_team=b.find_all(class_='ds-text-tight-l ds-font-bold ds-text-typo hover:ds-text-typo-primary ds-block ds-truncate')[0].text
+batting_team=b.find_all(class_='ds-text-tight-l ds-font-bold ds-text-typo hover:ds-text-typo-primary ds-block ds-truncate')[1].text
+venue=b.find(class_='ds-flex ds-items-center').text.split(',')[1]
+list=[]
+list1=[]
+list2=[]
+list3=[]
+list4=[]
+list5=[]
+list6=[]
+#print(b.find_all(class_='ds-text-tight-s ds-font-regular ds-flex ds-justify-center ds-items-center ds-w-7 ds-h-7 ds-rounded-full ds-border ds-border-ui-stroke ds-bg-fill-content-prime')[49].text)
+elements = b.find_all(class_='ds-cursor-pointer ds-pt-1')
+
+for i, element in enumerate(elements):
+    if element.text.split('/') is None:
+        print(' ')
+    else:
+        if i % 2 != 0:
+            list.append(element.text.split('/')[0])
+            list1.append(element.text.split('/')[1].split('(')[0])
+
+for i in range(len(list)):
+    list2.append(b.find_all(class_='ds-text-tight-s ds-font-regular ds-flex ds-justify-center ds-items-center ds-w-7 ds-h-7 ds-rounded-full ds-border ds-border-ui-stroke ds-bg-fill-content-prime')[i].text)
+    list3.append(b.find(class_='ds-text-compact-m ds-text-typo ds-text-right ds-whitespace-nowrap').text.split('/')[0])
+    list4.append(b.find_all(class_='ds-text-tight-l ds-font-bold ds-text-typo hover:ds-text-typo-primary ds-block ds-truncate')[0].text)
+    list5.append(b.find_all(class_='ds-text-tight-l ds-font-bold ds-text-typo hover:ds-text-typo-primary ds-block ds-truncate')[1].text)
+    list6.append(b.find(class_='ds-flex ds-items-center').text.split(',')[1])
+
+dict = {'batting_team': list4, 'bowling_team': list5,'venue':list6,'score':list,'wickets':list1,'over':list2,'target':list3} 
+df=pd.DataFrame(dict)
+df['score']=df['score'].astype('int')
+df['over']=df['over'].astype('int')
+df['wickets']=df['wickets'].astype('int')
+df['target']=df['target'].astype('int')
+df['runs_left']=df['target']-df['score']
+df=df[df['score']<df['target']]
+df['crr']=(df['score']/df['over'])
+df['rrr']=((df['target']-df['score'])/(50-df['over']))
+df['balls_left']=300-(df['over']*6)
+df['runs'] = df['score'].diff()
+df['last_10']=df['runs'].rolling(window=10).sum()
+df['wickets_in_over'] = df['wickets'].diff()
+df['last_10_wickets']=df['wickets_in_over'].rolling(window=10).sum()
+
+df=df.dropna()
+df
 if l is None:
     st.write('No Match Available')
 else:
     temp_df, target = match_progression(df, l, pipe)
     if temp_df is None:
         st.write("Error: Match is not Existed")
-
-
 
 # In[271]:
 v=temp_df['venue'].unique()
