0c781f0b6159da91dcb609e84ea3cd9112e2eb6b
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-01 21:26:31 +0530

    Update score.py

--- score.py
+++ score.py
@@ -579,7 +579,7 @@ wicket_y = temp_df['runs_after_over'] + temp_df['wickets_in_over'] * 1  # adjust
 wicket_y[wicket_y == temp_df['runs_after_over']] = None  # hide scatter points for 0 wickets
 wicket = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=wicket_y,  # use adjusted y-position
                                   mode='markers', name='Wickets in Over',
-                                  marker_color='orange', ,marker_size=10,
+                                  marker_color='orange',marker_size=10,
                                   text=wicket_text, textposition='top center'))
 # Line plots for batting and bowling teams
 batting_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting team',line_color='green', line_width=4))
