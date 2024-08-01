94cb9cfb3e72172100f80b03e259b9c343d5c05a
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-01 20:16:01 +0530

    Update score.py

--- score.py
+++ score.py
@@ -580,7 +580,7 @@ fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='line
 
 fig1.update_yaxes(range=[0, 100], tickvals=[0, midpoint, 100], ticktext=[a2, '50%', b2])
 
-fig1.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over', marker=dict(color='purple')))
+fig1.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over'))
 
 wicket_text = temp_df['wickets_in_over'].astype(str)
 wicket_y = temp_df['runs_after_over'] + temp_df['wickets_in_over'] * 1  # adjust y-position based on wickets
