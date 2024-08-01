7a9adbca6216e4c11ee55f04288e7a915257f618
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-01 20:00:32 +0530

    Update score.py

--- score.py
+++ score.py
@@ -573,8 +573,7 @@ a2=gf['bowling_team'].unique()
 b2=gf['batting_team'].unique()
 
 # Line chart for batting and bowling teams
-fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines',
-                    line=dict(color='yellow', width=3),name='Probablity'))
+fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines',line=dict(color='yellow', width=3),name='Probablity'))
 fig1.update_yaxes(range=[0, 100], tickvals=[0, midpoint, 100], ticktext=[a2, '50%',b2])
 runs = fig1.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over',marker=dict(color='purple')))
 wicket_text = temp_df['wickets_in_over'].astype(str)
