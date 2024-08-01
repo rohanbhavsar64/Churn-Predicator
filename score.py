f70e7412e2878f84fbafa19c5141f6345dac7305
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-01 22:21:55 +0530

    Update score.py

--- score.py
+++ score.py
@@ -498,7 +498,8 @@ pipe.fit(X_train,y_train)
 
 
 n=pipe.predict_proba(pd.DataFrame(columns=['batting_team','bowling_team','venue','score','wickets','runs_left','balls_left','crr','rrr','last_10','last_10_wicket'],data=np.array(['India','Australia','Punjab Cricket Association Stadium, Mohali',185,4,93,108,5.6,5.41,42.0,3.0]).reshape(1,11))).astype(float)
-
+g=gf[gf['runs']<0].index()
+gf=gf[:k,:]
 import streamlit as st
 
 def match_progression(x_df,match_id,pipe):
