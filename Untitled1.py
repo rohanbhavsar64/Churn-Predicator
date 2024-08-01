91c288f92552c5486a48bd165368badea7943354
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-01 19:14:37 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -440,7 +440,7 @@ pipe.fit(X_train,y_train)
 
 n=pipe.predict_proba(pd.DataFrame(columns=['batting_team','bowling_team','venue','score','wickets','runs_left','balls_left','crr','rrr','last_10','last_10_wicket'],data=np.array(['India','Australia','Punjab Cricket Association Stadium, Mohali',185,4,93,108,5.6,5.41,42.0,3.0]).reshape(1,11))).astype(float)
 match=match[match['date']>'2019-01-01']
-df2.to_csv()
+
 import streamlit as st
 st.header('2019 ODI WORLD CUP ANALYSIS')
 # In[255]:
