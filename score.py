1c404522a54a36b0fc6ac7acdbc7ba525dac1e41
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-01 20:28:23 +0530

    Update score.py

--- score.py
+++ score.py
@@ -571,12 +571,11 @@ b2=gf['batting_team'].unique()
 
 # Line chart for batting and bowling teams
 import plotly.graph_objects as go
+fig = go.Figure()
+runs = fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over'))
 
-fig1 = go.Figure()  # Create a figure object
-fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name=b2))
-fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name=a2))
-
-fig1.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over'))
-
-
-st.write(fig1)
+# Line plots for batting and bowling teams
+batting_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name=b2))
+bowling_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name=a2,))
+fig.update_layout(title='Target-' + str(target))
+st.write(fig)
