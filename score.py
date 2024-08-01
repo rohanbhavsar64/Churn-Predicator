eb935e01f2636a18d354ea94855e38b8162e8de7
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-01 20:22:42 +0530

    Update score.py

--- score.py
+++ score.py
@@ -548,10 +548,7 @@ import plotly.express as px
                                             #text=wicket_text, textposition='top center'))
 
 # Line plots for batting and bowling teams
-        #batting_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting Team',
-                                           # line=dict(color='#00a65a', width=3)))
-        #bowling_team = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name='Bowling Team',
-                                            #line=dict(color='red', width=4)))
+        
         #fig.update_layout(
           #  title='Target-' + str(target),
          #   width=800,  # Set the width of the chart
@@ -576,9 +573,8 @@ b2=gf['batting_team'].unique()
 import plotly.graph_objects as go
 
 fig1 = go.Figure()  # Create a figure object
-fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Probability'))
-
-fig1.update_yaxes(range=[0, 100], tickvals=[0, midpoint, 100], ticktext=[a2, '50%', b2])
+batting_team = fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name=b2))
+bowling_team = fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name=a2))
 
 fig1.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over'))
 
