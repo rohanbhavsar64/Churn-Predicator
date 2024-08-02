18bb2b1c233cbb82b2fd7341a89c558ae686178d
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-02 15:23:50 +0530

    Update score.py

--- score.py
+++ score.py
@@ -70,7 +70,7 @@ df=df.fillna(0)
 df['match_id']=100001
 import plotly.graph_objects as go
 fig = go.Figure(data=[
-    go.Scatter(x=df1['over'], y=df1['inng1'], name=df['bowling_team'].unique()[0], line_color='red')
+    go.Scatter(x=df1['over'], y=df1['inng1'], name=df['bowling_team'].unique()[0], line_color='red'),
     go.Scatter(x=df['over'], y=df['score'], name=df['batting_team'].unique()[0], line_color='grren')
 ])
 
