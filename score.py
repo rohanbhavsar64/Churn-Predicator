8344358a538a8cfc603f36c2b121282517b25390
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-03 19:17:54 +0530

    Update score.py

--- score.py
+++ score.py
@@ -74,8 +74,8 @@ if not neg_idx.empty:
 lf=df
 lf=lf[:int(o)]
 st.subheader('Scorecard')
-st.write(df['batting_team'].unique()[0]+' '+str(df['target'].unique()[0]))
-st.write(df['bowling_team'].unique()[0]+' '+str(df.iloc[-1,3]))
+st.write(f'**df['batting_team'].unique()[0]** :'+str(df['target'].unique()[0]))
+st.write(f'**df['bowling_team'].unique()[0]** :'+str(df.iloc[-1,3]))
 
 
 import plotly.graph_objects as go
