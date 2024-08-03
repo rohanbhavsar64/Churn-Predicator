ce8f8b40f8d82264361ecee0a25887427cb83183
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-03 19:46:08 +0530

    Update score.py

--- score.py
+++ score.py
@@ -81,7 +81,7 @@ if o!=50:
         st.write(f"**{df['bowling_team'].unique()[0]}**")
         st.write(f"**{df['batting_team'].unique()[0]}**")
     with col2:
-        st.write(df['target'].unique()[0])
+        st.write(str(df['target'].unique()[0]))
         st.write(str(df.iloc[o, 3])+'/'+str(df.iloc[o, 4]))
     st.text('crr : '+str(df.iloc[o,9].round(2))+'rrr : '+str(df.iloc[o,10].round(2)))
     st.write(df['batting_team'].unique()[0]+' Required '+str(df.iloc[o,8])+' runs in '+str(df.iloc[o,11])+' balls')
