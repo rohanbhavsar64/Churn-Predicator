546733a1a022ded4e0afd6e8031a8fd4b6244ad7
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-05 11:34:28 +0530

    Update score.py

--- score.py
+++ score.py
@@ -637,7 +637,7 @@ if o!=50:
 fig1=go.Figure()
 runs = fig1.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over',marker_color='purple'))
 wicket_text = temp_df['wickets_in_over'].astype(str)
-wicket_y = temp_df['runs_after_over']+temp_df['wickets_in_over']*1-1  # adjust y-position based on wickets
+wicket_y = temp_df['runs_after_over']+temp_df['wickets_in_over']*1  # adjust y-position based on wickets
 wicket_y[wicket_y == temp_df['runs_after_over']] = None  # hide scatter points for 0 wickets
 wicket = fig1.add_trace(go.Scatter(x=temp_df['end_of_over'], y=wicket_y,  # use adjusted y-position
                                   mode='markers', name='Wickets in Over',
