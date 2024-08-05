eb2e89479335dd36b46c5e868215c5750c7ebc62
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-05 11:24:43 +0530

    Update score.py

--- score.py
+++ score.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests 
 import streamlit as st
 st.header('ODI MATCH ANALYSIS')
-o=st.number_input('Over No.') or 50
+o=st.number_input('Over No.') or 40
 h = st.text_input('URL OF match-overs-comparison OF ESPN CRICINFO') or 'https://www.espncricinfo.com/series/icc-cricket-world-cup-2023-24-1367856/australia-vs-south-africa-2nd-semi-final-1384438/match-overs-comparison'
 if (h=='https://www.espncricinfo.com/series/icc-cricket-world-cup-2023-24-1367856/australia-vs-south-africa-2nd-semi-final-1384438/match-overs-comparison'):
     st.write('Enter Your URL')
@@ -655,18 +655,13 @@ fig.update_layout(
 fig.update_layout(title='Probablity Of Teams :Target-' + str(target))
 
 st.write(fig)
-def result(raw):
-    return 1 if(raw['batting_team']==raw['winner']) else 0
-
-
-# In[230]:
-
-
-gf['winner']=gf.apply(result,axis=1)
-
-if ((h=='https://www.espncricinfo.com/series/icc-cricket-world-cup-2023-24-1367856/australia-vs-south-africa-2nd-semi-final-1384438/match-overs-comparison') or (o!=50)):
-    new_df=new_df
-else:
-    new_df = pd.concat([new_df, gf[['batting_team','bowling_team','venue','score','wickets','runs_left','balls_left','crr','rrr','last_10','last_10_wicket','winner']]])
+if o==50:
+    def result(raw):
+        return 1 if(raw['batting_team']==raw['winner']) else 0
+        gf['winner']=gf.apply(result,axis=1)
+        if ((h=='https://www.espncricinfo.com/series/icc-cricket-world-cup-2023-24-1367856/australia-vs-south-africa-2nd-semi-final-1384438/match-overs-comparison') or (o!=50)):
+            new_df=new_df
+        else:
+            new_df = pd.concat([new_df, gf[['batting_team','bowling_team','venue','score','wickets','runs_left','balls_left','crr','rrr','last_10','last_10_wicket','winner']]])
 new_df.drop_duplicates(inplace=True)
 
