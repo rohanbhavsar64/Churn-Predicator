a8fc0fe3494581b0d9ec0ad9c822cbbb0251a6c2
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-08-06 12:53:23 +0530

    Update score.py

--- score.py
+++ score.py
@@ -113,7 +113,7 @@ l2=[]
 if o==50:
     lf['over_group'] = lf['over'] // 10
     lf_grouped = lf.groupby('over_group')['runs'].sum().reset_index()
-    fig2 = px.pie(data_frame=lf_grouped, names='over_group', values='runs',text='runs', hover_name='over_group', hover_data=['runs'])
+    fig2 = px.pie(data_frame=lf_grouped, names='over_group', values='runs', hover_name='over_group', hover_data=['runs'])
     fig2.update_layout(title_text="Runs per 10 Overs")
     st.write(fig2)
     
