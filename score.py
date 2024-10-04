bbba83a250967d0f715c7cb8700fb3c7026bd2d7
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-10-04 09:56:11 +0530

    Update score.py

--- score.py
+++ score.py
@@ -45,8 +45,8 @@ df1=pd.DataFrame(dict1)
 for i in range(len(list)):
     list2.append(b.find_all(class_='ds-text-tight-s ds-font-regular ds-flex ds-justify-center ds-items-center ds-w-7 ds-h-7 ds-rounded-full ds-border ds-border-ui-stroke ds-bg-fill-content-prime')[i].text)
     list3.append(b.find(class_='ds-text-compact-m ds-text-typo ds-text-right ds-whitespace-nowrap').text.split('/')[0])
-    list4.append(b.find_all(class_='ds-bg-fill-content-alternate ds-text-left')[1].text)
-    list5.append(b.find_all(class_='ds-bg-fill-content-alternate ds-text-left')[2].text)
+    list4.append(b.find_all(class_='ds-bg-fill-content-prime ds-overflow-hidden ds-mb-4 ds-rounded-lg')[4].text)
+    list5.append(b.find_all(class_='ds-bg-fill-content-prime ds-overflow-hidden ds-mb-4 ds-rounded-lg')[5].text)
     list6.append(b.find(class_='ds-flex ds-items-center').text.split(',')[1])
     if o==50:
         list7.append(b.find(class_='ds-text-tight-s ds-font-medium ds-truncate ds-text-typo').text.split(' ')[0])
