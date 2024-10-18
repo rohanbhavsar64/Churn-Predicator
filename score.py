1ec07f1f528ec211f8cd4d9ca44b94fa81e04e4c
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-10-18 18:50:07 +0530

    Update score.py

--- score.py
+++ score.py
@@ -141,7 +141,7 @@ if o>40:
     fig = go.Figure(data=[go.Pie(values=hj['slice'], labels=hj['Runs'].astype(str) + '/' + hj['Wickets'].astype(str), 
                              texttemplate='%{label}', textinfo='label',hoverinfo='none', 
                              marker_colors=colors,pull=[0.05, 0.05, 0.05, 0.05])])
-    fig.update_layout(title_text='Session Distribution',showlegend=False)
+    fig.update_layout(title_text='Session Distribution')
     st.write(fig)
     
 
